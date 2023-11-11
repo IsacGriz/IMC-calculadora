335ce05a2a34be1ee64b8042ccb26debf16b2694
isacgriz <isacgrizante18@gmail.com>
2023-11-11 20:14:21 -0300

    Resolvendo problemas com alturas iguais a 0 ou acima de 10

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,11 +7,17 @@ function imc () {
   resultado.innerHTML = '';
 
   function imcCalculo (evento) {
-    evento.preventDefault();
+    evento.preventDefault();  
+
+    if (altura.value >= 10 && altura.value < 100) {
+      altura.value /= 10;
+    } else if (altura.value >= 100) {
+      altura.value /= 100;
+    }
     
     imc = ((peso.value).replace(',', '.') / ((altura.value).replace(',', '.') ** 2)).toFixed(2);
 
-    if (imc < 18.5) {
+    if (imc > 0 && imc < 18.5) {
       resultado.innerHTML = `<p>O seu IMC é de ${imc} e você está abaixo do peso!</p>`;
     } else if (imc >= 18.5 && imc <= 24.9) {
       resultado.innerHTML = `<p>O seu IMC é de ${imc} e você está com o peso normal!</p>`;
