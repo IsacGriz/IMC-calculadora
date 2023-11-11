3ebe9ad089398a1ed451316b3dade4c55090c366
isacgriz <isacgrizante18@gmail.com>
2023-11-11 20:39:41 -0300

    tratamento de Infinity e caracteres inválidos

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,20 +4,23 @@ function imc () {
   let altura = form.querySelector('#altura');
   const resultado = document.querySelector('#paragrafo');
 
-  resultado.innerHTML = '';
-
   function imcCalculo (evento) {
     evento.preventDefault();  
 
     if (altura.value >= 10 && altura.value < 100) {
       altura.value /= 10;
-    } else if (altura.value >= 100) {
+    } else if (altura.value >= 100 && imc < 1000) {
       altura.value /= 100;
+    } else if (altura.value >= 1000 || peso.value >= 1000) {
+      altura.value = 0;
+      peso,value = 0;
     }
-    
+
     imc = ((peso.value).replace(',', '.') / ((altura.value).replace(',', '.') ** 2)).toFixed(2);
 
-    if (imc > 0 && imc < 18.5) {
+    if (imc == Infinity) {
+      resultado.innerHTML = 'Números inválidos!'
+    } else if (imc > 0 && imc < 18.5) {
       resultado.innerHTML = `<p>O seu IMC é de ${imc} e você está abaixo do peso!</p>`;
     } else if (imc >= 18.5 && imc <= 24.9) {
       resultado.innerHTML = `<p>O seu IMC é de ${imc} e você está com o peso normal!</p>`;
@@ -29,6 +32,8 @@ function imc () {
       resultado.innerHTML = `<p>O seu IMC é de ${imc} e você está com obesidade de grau 2!</p>`;
     } else if (imc >= 40) {
       resultado.innerHTML = `<p>O seu IMC é de ${imc} e você está com obesidade de grau 3!</p>`;
+    } else if (imc === Infinity) {
+      resultado.innerHTML = 'Valores inválidos';
     } else {
       resultado.innerHTML = 'Você digitou caracteres inválidos! Verifique e tente novamente!';
     }
